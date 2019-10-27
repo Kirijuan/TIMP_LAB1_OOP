cc89a44f622ffac425696add91ee6ee31c3b8993
Кирилл Селютин <kir.seliutin1@outlook.com>
2019-10-27 21:57:55 +0700

    Добавьте файлы проекта.

---
+++ TIMP_LAB1_OOP/circle_In.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "stdafx.h"
+#include "circle_atd.h"
+using namespace std;
+namespace simple_shapes {
+	// ¬вод параметров треугольника
+	void circle::InData(ifstream &ifst) {
+		ifst >> x >> R >> colour;
+	}
+} // end simple_shapes namespace
---
+++ TIMP_LAB1_OOP/rectangle_In.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "stdafx.h"
+#include "rectangle_atd.h"
+using namespace std;
+namespace simple_shapes {
+	// ¬вод параметров пр€моугольника
+	void rectangle::InData(ifstream &ifst) 
+	{
+		ifst >> x >> y >> colour;
+	}
+} // end simple_shapes namespace
